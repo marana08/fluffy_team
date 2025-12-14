8942e4bed4154947bd838f05d9be9425370b8271
ivanna_shchokalo <ivanka.m78@gmail.com>
2025-12-14 21:50:07 +0100

    Complete pets section functionality

---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export const refs = {
+    categoryList: document.querySelector('.js-category-list'),
+    petsList: document.querySelector('.js-pets-list'),
+    loader: document.querySelector('.js-loader'),
+    petsLoadMoreBtn: document.querySelector('.js-pets-load-more-btn')
+}
