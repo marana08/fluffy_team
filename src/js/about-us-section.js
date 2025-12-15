7627179653e7d8cc35a386916b541df9790bd575
Maksym Lutsiak <demon164320@gmail.com>
2025-12-15 18:20:53 +0100

    fix

--- src/js/about-us-section.js
+++ src/js/about-us-section.js
@@ -1,23 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
 const aboutSwiper = new Swiper('.about-slider-wrapper', {
     slidesPerView: 1,
     spaceBetween: 16, // відстань між слайдами
 
     navigation: {
-        nextEl: '.swiper-button-next', // кнопка вперед
-        prevEl: '.swiper-button-prev', // кнопка назад
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
 
     pagination: {
-        el: '.swiper-pagination', // пагінація
+        el: '.swiper-pagination',
         clickable: true,
     },
 
     breakpoints: {
         768: {
-            spaceBetween: 24, // для планшетів
-        },
+        spaceBetween: 24, // для планшетів
+    },
         1440: {
-            spaceBetween: 32, // для десктопів
-        }
+        spaceBetween: 32, // для десктопів
+    }
     }
 });
+});
+
+document.querySelector('.swiper-pagination').style.position = 'static';
