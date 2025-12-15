f429e75b241e23d6ccb6cd854cce2019911e8f59
damirmamian <damirmamian@gmail.com>
2025-12-16 00:21:22 +0200

    solved issues sand made js

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,28 @@
-// Хедер
+const menuButton = document.querySelector('.header-burger-menu');
+const modal = document.querySelector(".header-modal")
+const closeButton = document.querySelector(".modal-close-button")
+const link = document.querySelector(".modal-navigation-item")
+const nav = document.querySelector(".modal-navigation-list")
+const takeButton = document.querySelector(".modal-button")
+
+function closeModal(event) {
+    modal.classList.remove("is-open")
+}
+
+document.addEventListener("keydown", (event) => {
+    const isEscape = event.key === "Escape";
+    const isModalOpen = modal.classList.contains("is-open");
+    if (isEscape && isModalOpen) {
+        modal.classList.remove("is-open");
+    }
+});
+
+takeButton.addEventListener("click", closeModal)
+
+menuButton.addEventListener("click", (event) => {
+    event.preventDefault()
+    modal.classList.add("is-open")
+})
+
+closeButton.addEventListener("click", closeModal)
+nav.addEventListener("click", closeModal)
