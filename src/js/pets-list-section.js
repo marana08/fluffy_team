b9c4498294cd9e7766373264678a1cdc6df9119b
Ivanna Shchokalo <119056668+ivannashchokalo@users.noreply.github.com>
2025-12-17 14:14:58 +0100

    fix pets list (#26)
    * add additional pagination feature
    
    * add aria-labes to pagination buttons
    
    * Update pagination button hover/focus and transition
    
    * fix images sizes and add js functionality
    
    * fix images path
    
    * Fix bugs
    
    * fix pets list
    
    * fix pets list css
    
    ---------
    
    Co-authored-by: mariana_melen <maranamelen13@gmail.com>

--- src/js/pets-list-section.js
+++ src/js/pets-list-section.js
@@ -3,21 +3,14 @@ import iziToast from "izitoast";
 import { ENDPOINTS, server } from "./server-api";
 import { refs } from "./refs";
 import { loadFromLS, saveToLS } from "./storage";
-import spriteUrl from '/img/sprite.svg';
-
-
 let limit = getLimitByScreen();
-let page = loadFromLS('page') || 1;
+let page = loadFromLS(‘page’) || 1;
 let totalItems;
-let categoryId = loadFromLS('categoryId') ?? null;
-
-document.addEventListener('DOMContentLoaded', handleContentLoad);
-refs.petsLoadMoreBtn.addEventListener('click', handleLoadMoreBtnClick);
-refs.categoryList.addEventListener('click', handleCategoryBtnClick);
-refs.petsListPagination.addEventListener('click', handlePaginationClick);
-
-
-
+let categoryId = loadFromLS(‘categoryId’) ?? null;
+document.addEventListener(‘DOMContentLoaded’, handleContentLoad);
+refs.petsLoadMoreBtn.addEventListener(‘click’, handleLoadMoreBtnClick);
+refs.categoryList.addEventListener(‘click’, handleCategoryBtnClick);
+refs.petsListPagination.addEventListener(‘click’, handlePaginationClick);
 function getLimitByScreen() {
   const width = window.innerWidth;
   if (width >= 1440) return 9;
@@ -26,170 +19,190 @@ function getLimitByScreen() {
 function getTotalPages() {
   return Math.ceil(totalItems / limit);
 }
-
 // --------------- handlers -------------------
 async function handleContentLoad(e) {
-  showLoader();
-  
-  try {
-      if (!categoryId) {
-          const categories = await fetchAllCategories();
-          const animals = await fetchAllAnimals();   
-          
-          renderCategories(categories);
-          renderAnimals(animals);
-          renderPagination();
-          checkLoadMoreBtnStatus();
-
-      } else {
-          const categories = await fetchAllCategories();
-          const animals = await fetchCategoryById(categoryId, page);
-          renderCategories(categories);
-          renderAnimals(animals);
-          renderPagination();
-          checkLoadMoreBtnStatus();
-  }
-  } catch (error) {
-      iziToast.error({
-          title: 'Помилка',
-          message: 'Щось пішло не так',
-          position: 'topRight',
-      })
-  } finally {
-      hideLoader();
-  }
-}
-
-async function handleCategoryBtnClick(e) {
-  if (e.target.nodeName !== 'BUTTON') return;
-  const categoryName = e.target.textContent;
-  const categoryLi = e.target.closest('li');
-  categoryId = categoryLi.dataset.id; 
-  page = 1;
-  let animals;
-  showLoader();
-
-  const allButtons = refs.categoryList.querySelectorAll('.category-btn');
-  allButtons.forEach(btn => {
-    btn.classList.remove('current');
-  });
-  
-  e.target.classList.add('current');
-
-  try {
-    if (categoryName !== 'Всі') {
-      animals = await fetchCategoryById(categoryId, page);
-    } else {
-      categoryId = null;
-      animals = await fetchAllAnimals();
+    showLoader();
+    try {
+        if (!categoryId) {
+            const categories = await fetchAllCategories();
+            const animals = await fetchAllAnimals();
+            renderCategories(categories);
+            renderAnimals(animals);
+            renderPagination();
+            chec
+            LoadMoreBtnStatus();
+        } else {
+            const categories = await fetchAllCategories();
+            const animals = await fetchCategoryById(categoryId, page);
+            renderCategories(categories);
+            renderAnimals(animals);
+            renderPagination();
+            checkLoadMoreBtnStatus();
     }
-      renderAnimals(animals);    
-      checkLoadMoreBtnStatus(); 
-      renderPagination();
-  } catch (error) {
-      iziToast.error({
-          title: 'Помилка',
-          message: 'Щось пішло не так',
-          position: 'topRight',
-      })
-  } finally {
-      hideLoader();
-      saveToLS('categoryId', categoryId);
-      saveToLS('page', page);
-  }
+    } catch (error) {
+       iziToast.error({
+            title: ‘Помилка‘,
+import axios from "axios";
+import iziToast from "izitoast";
+import { ENDPOINTS, server } from "./server-api";
+import { refs } from "./refs";
+import { loadFromLS, saveToLS } from "./storage";
+let limit = getLimitByScreen();
+let page = loadFromLS("page") || 1;
+let totalItems;
+let categoryId = loadFromLS("categoryId") ?? null;
+document.addEventListener("DOMContentLoaded", handleContentLoad);
+refs.petsLoadMoreBtn.addEventListener("click", handleLoadMoreBtnClick);
+refs.categoryList.addEventListener("click", handleCategoryBtnClick);
+refs.petsListPagination.addEventListener("click", handlePaginationClick);
+function getLimitByScreen() {
+  const width = window.innerWidth;
+  if (width >= 1440) return 9;
+  return 8;
+}
+function getTotalPages() {
+  return Math.ceil(totalItems / limit);
+}
+// --------------- handlers -------------------
+async function handleContentLoad(e) {
+    showLoader();
+    try {
+        if (!categoryId) {
+            const categories = await fetchAllCategories();
+            const animals = await fetchAllAnimals();
+            renderCategories(categories);
+            renderAnimals(animals);
+            renderPagination();
+            checkLoadMoreBtnStatus();
+        } else {
+            const categories = await fetchAllCategories();
+            const animals = await fetchCategoryById(categoryId, page);
+            renderCategories(categories);
+            renderAnimals(animals);
+            renderPagination();
+            checkLoadMoreBtnStatus();
+    }
+    } catch (error) {
+       iziToast.error({
+            title: "Помилка",
+            message: "Щось пішло не так",
+            position: "topRight",
+        })
+    } finally {
+        hideLoader();
+    }
+}
+async function handleCategoryBtnClick(e) {
+    if (e.target.nodeName !== "BUTTON") return;
+    const categoryName = e.target.textContent;
+    const categoryLi = e.target.closest("li");
+    categoryId = categoryLi.dataset.id;
+    page = 1;
+    showLoader();
+    const allButtons = refs.categoryList.querySelectorAll(".category-btn");
+    allButtons.forEach(btn => {
+        btn.classList.remove("current");
+    });
+    e.target.classList.add("current");
+    try {
+        let animals = await fetchAllAnimals();
+        if (categoryName !== "Всі") {
+            animals = await fetchCategoryById(categoryId, page);
+        }
+        renderAnimals(animals);
+        checkLoadMoreBtnStatus();
+        renderPagination();
+    } catch (error) {
+        iziToast.error({
+            title: "Помилка",
+            message: "Щось пішло не так",
+            position: "topRight",
+        })
+    } finally {
+        hideLoader();
+        saveToLS("categoryId", categoryId);
+        saveToLS("page", page);
+    }
 }
 async function handleLoadMoreBtnClick() {
     page += 1;
-    refs.loader.classList.add('loader-center');
+    refs.loader.classList.add("loader-center");
     showLoader();
     try {
         if (!categoryId) {
             checkLoadMoreBtnStatus();
             const animals = await fetchAllAnimals(page);
             const markup = animalsTemplate(animals);
-            refs.petsList.insertAdjacentHTML('beforeend', markup);
+            refs.petsList.insertAdjacentHTML("beforeend", markup);
         } else {
             checkLoadMoreBtnStatus();
             const animals = await fetchCategoryById(categoryId, page);
             const markup = animalsTemplate(animals);
-            refs.petsList.insertAdjacentHTML('beforeend', markup);
+            refs.petsList.insertAdjacentHTML("beforeend", markup);
         }
-        const firstCard = refs.petsList.querySelector('li');
+        const firstCard = refs.petsList.querySelector("li");
         if (firstCard) {
             const cardRect = firstCard.getBoundingClientRect();
             window.scrollBy({
                 top: cardRect.height,
-                behavior: 'smooth',
+                behavior: "smooth",
             });
         }
     } catch (error) {
         iziToast.error({
-            title: 'Помилка',
-            message: 'Щось пішло не так',
-            position: 'topRight',
+            title: "Помилка",
+            message: "Щось пішло не так",
+            position: "topRight",
         })
     } finally {
         hideLoader();
-        saveToLS('page', page);
-        refs.loader.classList.remove('loader-center');
-    }  
+        saveToLS("page", page);
+        refs.loader.classList.remove("loader-center");
+    }
 }
-
 async function handlePaginationClick(e) {
-    const btn = e.target.closest('button');
+    const btn = e.target.closest("button");
     if (!btn) return;
-
     let animals;
-
     const totalPages = getTotalPages();
-
-    if (btn.dataset.action === 'prev' && page > 1) {
+    if (btn.dataset.action === "prev" && page > 1) {
         page -= 1;
     }
-
-    if (btn.dataset.action === 'next' && page < totalPages) {
+    if (btn.dataset.action === "next" && page < totalPages) {
         page += 1;
     }
-
     if (btn.dataset.page) {
         page = Number(btn.dataset.page);
     }
-
     showLoader();
     try {
         if (categoryId) {
             animals = await fetchCategoryById(categoryId, page);
         } else {
          animals = await fetchAllAnimals(page);
-        }   
-
+        }
         renderAnimals(animals);
         renderPagination();
         window.scrollTo({
-            top: refs.petsList.offsetTop - 80,
-            behavior: 'smooth',
+            top: refs.petsList.offsetTop - 160,
+            behavior: "smooth",
         });
-
         } catch (error) {
             iziToast.error({
-                title: 'Помилка',
-                message: 'Щось пішло не так',
-                position: 'topRight',
+                title: "Помилка",
+                message: "Щось пішло не так",
+                position: "topRight",
             })
         } finally {
             hideLoader();
-            saveToLS('page', page);
+            saveToLS("page", page);
         }
 }
-
-
 // ----------------- API -----------------
-
 async function fetchAllCategories(page) {
-    const response = await server.get(`${ENDPOINTS.categories}`);   
+    const response = await server.get(`${ENDPOINTS.categories}`);
     return response.data;
 }
-
 async function fetchAllAnimals(page) {
     const response = await server.get(`${ENDPOINTS.animals}`, {
         params: {
@@ -197,11 +210,9 @@ async function fetchAllAnimals(page) {
             page: page
         }
     });
-
     totalItems = response.data.totalItems;
-    return response.data.animals; 
+    return response.data.animals;
 }
-
 async function fetchCategoryById(id, page) {
     const response = await server.get(`${ENDPOINTS.animals}?categoryId=${id}`, {
         params: {
@@ -221,7 +232,7 @@ function categoryTemplate(category) {
       </li>`
 }
 function categoriesTemplate(categories) {
-    return categories.reverse().map(categoryTemplate).join('');
+    return categories.reverse().map(categoryTemplate).join("");
 }
 function renderCategories(categories) {
     const isAllActive = !categoryId;
@@ -233,7 +244,7 @@ function renderCategories(categories) {
 function animalTemplate({ _id, name, image, species, age, gender, categories, description }) {
     const categoriesMarkup = categories
         .map(category => `<li class="pets-category-item">${category.name}</li>`)
-        .join('');
+        .join("");
     return `
      <li class="pets-item" data-id="${_id}">
      <div class="pets-img-wrapper"><img class="pets-img" src="${image}" alt="${name} - ${species}" /></div>
@@ -253,27 +264,23 @@ function animalTemplate({ _id, name, image, species, age, gender, categories, de
       </li>`
 }
 function animalsTemplate(animals) {
-    return animals.map(animalTemplate).join('');
+    return animals.map(animalTemplate).join("");
 }
 function renderAnimals(animals) {
     const markup = animalsTemplate(animals);
     refs.petsList.innerHTML = markup;
 }
-
 function renderPagination() {
   const totalPages = getTotalPages();
   if (totalPages <= 1) return;
-
-  let markup = '';
-
+  let markup = "";
   markup += `<li>
-      <button class="pagination-btn-arrow" data-action="prev" aria-label="Попередня сторінка" ${page === 1 ? 'disabled' : ''}>
-        <svg class="arrow-icon" aria-hidden="true" width="24" height="24">
-          <use href="${spriteUrl}#icon-arrow-back"></use>
+      <button class="pagination-btn-arrow" data-action="prev aria-label="Попередня сторінка"" ${page === 1 ? 'disabled' : ''}>
+        <svg class="arrow-icon" width="24" height="24">
+          <use href="/img/sprite.svg#icon-arrow-back"></use>
         </svg>
       </button>
     </li>`;
-
   if (page === 1) {
     for (let i = 1; i <= Math.min(3, totalPages); i += 1) {
       markup += pageButton(i);
@@ -284,37 +291,30 @@ function renderPagination() {
     }
   } else {
       markup += pageButton(1);
-
     if (page > 3) {
       markup += `<li class="dots">…</li>`;
     }
-
     for (let i = page - 1; i <= page + 1; i += 1) {
       if (i > 1 && i < totalPages) {
         markup += pageButton(i);
       }
     }
-
     if (page < totalPages - 2) {
       markup += `<li class="dots">…</li>`;
     }
-
     if (totalPages > 1) {
       markup += pageButton(totalPages);
     }
   }
-
   markup += `<li>
       <button class="pagination-btn-arrow" data-action="next" aria-label="Наступна сторінка" ${page === totalPages ? 'disabled' : ''}>
-        <svg class="arrow-icon" aria-hidden="true" width="24" height="24">
-          <use href="${spriteUrl}#icon-arrow-forward"></use>
+        <svg class="arrow-icon" width="24" height="24">
+          <use href="/img/sprite.svg#icon-arrow-forward"></use>
         </svg>
       </button>
     </li>`;
-
   refs.petsListPagination.innerHTML = markup;
 }
-
 function pageButton(pageNumber) {
   return `
     <li>
@@ -327,20 +327,19 @@ function pageButton(pageNumber) {
     </li>
   `;
 }
-
 // --------------- loader ---------------
 function showLoader() {
-    refs.loader.classList.remove('loader-hidden');
+    refs.loader.classList.remove("loader-hidden");
 }
 function hideLoader() {
-    refs.loader.classList.add('loader-hidden');
+    refs.loader.classList.add("loader-hidden");
 }
 // --------------- load more btn ---------------
 function showLoadBtn() {
-    refs.petsLoadMoreBtn.classList.remove('pets-load-more-btn-hidden')
+    refs.petsLoadMoreBtn.classList.remove("pets-load-more-btn-hidden")
 }
 function hideLoadBtn() {
-    refs.petsLoadMoreBtn.classList.add('pets-load-more-btn-hidden')
+    refs.petsLoadMoreBtn.classList.add("pets-load-more-btn-hidden")
 }
 function checkLoadMoreBtnStatus() {
     const totalPages = getTotalPages();
@@ -350,5 +349,6 @@ function checkLoadMoreBtnStatus() {
         showLoadBtn();
     }
 }
+
 
 
